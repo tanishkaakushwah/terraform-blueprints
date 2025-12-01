7e1cd3552dcb2abcd3b54bc3d8c430be873bdcc8
Tanishka Kushwah <168574530+tanishkaakushwah@users.noreply.github.com>
2025-12-01 22:46:22 +0530

    Update SQL Server admin credentials

--- microservices_infra/modules/azurerm_sql_server/main.tf
+++ microservices_infra/modules/azurerm_sql_server/main.tf
@@ -3,6 +3,6 @@ resource "azurerm_mssql_server" "server" {
   resource_group_name          = var.rg_name
   location                     = var.rg_location
   version                      = "12.0"
-  administrator_login          = "admin"
-  administrator_login_password = "admin"
+  administrator_login          = "tani"
+  administrator_login_password = "tani"
 }
