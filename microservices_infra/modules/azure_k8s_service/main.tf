d2aa1cd504d654ae5513601dc7e8b8a602c4f577
Tanishka Kushwah <168574530+tanishkaakushwah@users.noreply.github.com>
2025-12-05 22:07:45 +0530

    Change VM size from Standard_D2ps_v6 to Standard_D2s_v3

--- microservices_infra/modules/azure_k8s_service/main.tf
+++ microservices_infra/modules/azure_k8s_service/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2ps_v6"
+    vm_size    = "Standard_D2s_v3"
   }
 
   identity {
